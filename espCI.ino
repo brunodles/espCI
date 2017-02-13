35fb337e9b00878b5128be6272dd3728b21d5bc6
Bruno de Lima <dlimaun@gmail.com>
2017-02-13 15:58:52 -0200

    Update espCI to create a default get method

--- espCI.ino
+++ espCI.ino
@@ -7,10 +7,15 @@
 
 #define USE_SERIAL Serial
 
+#define FIREBASE_URL "https://esp-ci.firebaseio.com"
+#define FIREBASE_FINGERPRINT "9A:E1:A3:B7:88:E0:C9:A3:3F:13:72:4E:B5:CB:C7:27:41:B2:0F:6A"
+
 ESP8266WiFiMulti WiFiMulti;
 
+int updateDelay = 10000;
+
 void setup() {
-  
+
   pinMode(0, OUTPUT);
   pinMode(2, OUTPUT);
   digitalWrite(0, 0);
@@ -29,7 +34,8 @@ void setup() {
         delay(1000);
     }
 
-    WiFiMulti.addAP("Lima", "password");
+    WiFiMulti.addAP("Lima", "pass");
+    WiFiMulti.addAP("CSVisitante", "pass");
     // todo Read Config file
 
 }
@@ -39,9 +45,10 @@ void loop() {
     if((WiFiMulti.run() == WL_CONNECTED)) {
       checkGPIO(0);
       checkGPIO(2);
+      checkConfig();
     }
 
-    delay(10000);
+    delay(updateDelay);
     // todo update config file
     // * get More Wifi-setting
     // * get get CI-urls
@@ -49,27 +56,43 @@ void loop() {
 
 void checkGPIO(int gpioIndex) {
     USE_SERIAL.printf("Update GPIO%d \n", gpioIndex);
-    
-    HTTPClient http;
-    String url = "https://esp-ci.firebaseio.com/gpio/gpio"+String(gpioIndex)+".json";
-    USE_SERIAL.println(url);
-    http.begin(url, "9A:E1:A3:B7:88:E0:C9:A3:3F:13:72:4E:B5:CB:C7:27:41:B2:0F:6A");
 
-    int httpCode = http.GET();
+    String path = "/gpio/gpio"+String(gpioIndex)+".json";
+    int value = firebaseGet(path).toInt();
 
     // httpCode will be negative on error
-    if(httpCode > 0) {
-      
-        if(httpCode == HTTP_CODE_OK) {
-            int payload = http.getString().toInt();
-            USE_SERIAL.printf("GPIO%d = %d\n", gpioIndex, payload);
-            digitalWrite(gpioIndex, payload);
-        }
+    if(value >= 0) {
+      USE_SERIAL.printf("GPIO%d = %d\n", gpioIndex, value);
+      digitalWrite(gpioIndex, value);
     } else {
-        USE_SERIAL.printf("[HTTP] GET... failed, error: %d %s\n", httpCode, http.errorToString(httpCode).c_str());
+        USE_SERIAL.println("Failed to update GPIO");
     }
+}
 
-    http.end();
+void checkConfig() {
+  int value = firebaseGet("/setup/delay.json").toInt();
+  if (value > 0) {
+    updateDelay = value;  
+  }
+}
+
+String firebaseGet(String path) {    
+  String url = FIREBASE_URL+path;
+  USE_SERIAL.print("GET ");
+  USE_SERIAL.println(url);
+  HTTPClient http;
+  http.begin(url, FIREBASE_FINGERPRINT);
   
+  int httpCode = http.GET();
+  String payload;
+//  if(httpCode > 0) {
+  if(httpCode == HTTP_CODE_OK) {
+      payload = http.getString();
+//    }
+  } else {
+    payload = "";
+  }
+  http.end();
+  return payload;  
 }
 
